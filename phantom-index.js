edf39335920ff2ccaf9c3be290ffe0e24cbdc74d
Matt Westhoff <mattwesthoff@gmail.com>
2013-01-09 23:33:27 -0600

    https to local doesn't work
    self signed cert I'm guessing

---
+++ phantom-index.js
@@ -0,0 +1,60 @@
+
+var page = require("webpage").create();
+var process = {env:{LOG_LEVEL: "WARN"}};
+var splunkjs = require("./splunk-sdk-javascript/index");
+
+
+// page.open("https://localhost:8089/service/auth/login?output_mode=json", 
+// 	"post", "username=admin&password=admin", function(status){
+// 		console.log(status);
+// 		phantom.exit();
+// })
+page.settings.localToRemoteUrlAccessEnabled = true;
+page.settings.webSecurityEnabled = false;
+page.open("http://localhost", function(status) {
+	console.log(status);
+	phantom.exit();
+});
+
+var PhantomHttp = splunkjs.Http.extend({
+        init: function() {
+            this._super();
+        },
+
+        makeRequest: function(url, message, callback) {
+        	console.log(url);
+        	console.log(message.body);
+        	console.log(message.method);
+        	page.open(url, message.method, message.body, function (status) {
+        		console.log(page.headers);
+        		console.log(status);
+        		callback({statusCode:status});
+        	});
+        },
+
+        parseJson: function(json) {
+           console.log(json);
+        }
+    });
+
+var http = new PhantomHttp();
+
+var service = new splunkjs.Service(http, {
+		username: "admin",
+    	password: "admin",
+    	scheme: "https",
+    	host: "localhost",
+    	port: "8089",
+    	version: "5.0"
+});
+
+// service.login(function(err, success) {
+// 	if (err || !success) {
+// 		console.error("couldn't log into splunk");
+// 		phantom.exit(1);
+// 	}
+
+// 	var logger = new Logger(service, {sourcetype: "testlogs", source: "test"});
+// 	logger.log({message:"This is a message for testing"});
+// 	phantom.exit();
+// });
