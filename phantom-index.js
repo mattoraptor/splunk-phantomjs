d32124e287bae3f27b3462409f1a4a4e83e25387
Matt Westhoff <mattwesthoff@gmail.com>
2013-01-10 14:17:18 -0600

    ok got some logging
    need to not exit until all logs are done

--- phantom-index.js
+++ phantom-index.js
@@ -1,7 +1,6 @@
 //this needs to be run with the --ignore-ssl-errors=yes option to work
 // on splunk apis with self-signed certs
 var process = {env:{LOG_LEVEL: "WARN"}};
-var logger = console;
 var splunkjs = require("./splunk-sdk-javascript/index");
 
 var getHeaders = function(headerArray) {
@@ -21,22 +20,27 @@ var PhantomHttp = splunkjs.Http.extend({
 
         makeRequest: function(url, message, callback) {
             var that = this;
-            console.log(url);
             var page = require("webpage").create();
             
             page.onResourceReceived = function(response) {
                 that.lastResponse = response;
             };
-        	
+        	page.customHeaders = message.headers;
             page.open(url, message.method, message.body, function (status) {
                 if (status === "success") {
-                    var response = {
-                        statusCode: that.lastResponse.status,
-                        headers: getHeaders(that.lastResponse.headers)
-                    };
-                    var complete_response = that._buildResponse(status, response, page.plainText);
-                    callback(complete_response);
-                 }
+                } else {
+                    console.log(url);
+                    console.log(status);
+                    console.log(JSON.stringify(message));
+                    console.log(JSON.stringify(page));
+                    console.log(JSON.stringify(that.lastResponse));
+                }
+                var response = {
+                    statusCode: that.lastResponse.status,
+                    headers: getHeaders(that.lastResponse.headers)
+                };      
+                var complete_response = that._buildResponse(status, response, page.plainText);
+                callback(complete_response);
             });
         },
 
@@ -56,16 +60,77 @@ var service = new splunkjs.Service(http, {
     	version: "5.0.1"
 });
 
+var Logger = splunkjs.Class.extend({
+    init: function(service, opts) {
+        this.service = service;
+        
+        opts = opts || {};
+        
+        this.params = {};
+        if (opts.index)      this.params.index      = opts.index;
+        if (opts.host)       this.params.host       = opts.host;
+        if (opts.source)     this.params.source     = opts.source;
+        if (opts.sourcetype) this.params.sourcetype = opts.sourcetype || "demo-logger";
+        
+        if (!this.service) {
+            throw new Error("Must supply a valid service");
+        }
+    },
+    
+    log: function(data) {
+        var message = {
+            __time: (new Date()).toUTCString(),
+            level: "LOG",
+            data: data
+        };
+        
+        this.service.log(message, this.params);
+        console.log(data);
+    },
+    
+    error: function(data) {
+        var message = {
+            __time: (new Date()).toUTCString(),
+            level: "ERROR",
+            data: data
+        };
+        
+        this.service.log(message, this.params);
+        console.error(data);
+    },
+    
+    info: function(data) {
+        var message = {
+            __time: (new Date()).toUTCString(),
+            level: "INFO",
+            data: data
+        };
+        
+        this.service.log(message, this.params);
+        console.info(data);
+    },
+    
+    warn: function(data) {
+        var message = {
+            __time: (new Date()).toUTCString(),
+            level: "WARN",
+            data: data
+        };
+        
+        this.service.log(message, this.params);
+        console.warn(data);
+    },
+});
+
 service.login(function(err, success) {
 	if (err || !success) {
         console.log(err);
 		console.error("couldn't log into splunk");
 		phantom.exit(1);
 	}
-    console.log("logged in");
-
-	//var logger = new Logger(service, {sourcetype: "testlogs", source: "test"});
-    console.log("logged in");
-	//logger.log({message:"This is a message for testing"});
-	phantom.exit();
+    
+	var logger = new Logger(service, {sourcetype: "testlogs", source: "test"});
+	logger.log("more test");
+    logger.info({"party":"cats", "awesome": "pizza"});
+	//phantom.exit();
 });
