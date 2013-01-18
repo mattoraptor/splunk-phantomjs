fe6fdb69a95af0902c279b8ddb1638e246a7c261
Matt Westhoff <mattwesthoff@gmail.com>
2013-01-18 15:10:45 -0600

    adding jasmine to wrap unit tests
    that's what we're using in our phantom tests I want to log to splunk
    anyway, that way you don't need node for this and we can still run w/o
    browser

---
+++ tests/testrunner.js
@@ -0,0 +1,50 @@
+
+//dependencies
+phantom.injectJs("./Jasmine/lib/jasmine-core/jasmine.js");
+phantom.injectJs("./Jasmine/src/console/ConsoleReporter.js");
+
+//tests
+describe("A specs", function() {
+	it("is a thing", function() {
+		expect(true);
+	})
+});
+
+//phantom.exit called when done
+var finishCallback = jasmine.Runner.prototype.finishCallback;
+jasmine.Runner.prototype.finishCallback = function() {
+	try { finishCallback.call(this); }
+	finally { phantom.exit(); }
+};
+
+//run tests with console.log output
+var jasmineEnv = jasmine.getEnv();
+jasmineEnv.addReporter(new jasmine.ConsoleReporter(function(str) { console.log(str); }));
+jasmineEnv.execute();
+
+
+// var process = {env:{LOG_LEVEL: "WARN"}};
+// var splunkjs = require("../phantom_index");
+
+// var http = new splunkjs.Phantom.Http.PhantomHttp();
+
+// var service = new splunkjs.Service(http, {
+// 		username: "admin",
+//     	password: "admin",
+//     	scheme: "https",
+//     	host: "localhost",
+//     	port: "8089",
+//     	version: "5.0.1"
+// });
+
+// service.login(function(err, success) {
+// 	if (err || !success) {
+//         console.log(err);
+// 		console.error("couldn't log into splunk");
+// 		phantom.exit(1);
+// 	}
+    
+// 	var logger = new splunkjs.Phantom.Logger.Logger(service, {sourcetype: "testlogs", source: "test"});
+//     logger.info({"party":"cats", "awesome": "tacos"});
+// 	//phantom.exit();
+// });
